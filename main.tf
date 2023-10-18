0e0c213bdd021b158989503e300b5e29ea913c80
Manmohan Batra <manmohan.batra@gmail.com>
2023-10-18 13:20:26 -0600

    updated main.tf

--- main.tf
+++ main.tf
@@ -1,25 +1,36 @@
 terraform {  
   backend "azurerm" {
+storage_account_name    = "tfex120"
+        resource_group_name     = "tfex120"
+        container_name          = "tfexc120" 
+        key                     = "terraform.tfstate"
+        access_key              = "2kQHCQ0+x7Nyf8WhMm3rG5UXptUx/Jbeceoz0XDncLzkZvsetb3Z3iCaSFXwLKRCj5UMr/cDGYVj+AStPWV3zA=="
     }
 }
 provider "azurerm" {
   features {}
 }
-resource "azurerm_app_service_plan" "main" {
+resource "azurerm_service_plan" "main" {
   name                = "docker-asp"
-  location            = "eastus"
   resource_group_name = "tfex120"
-  kind                = "Linux"
-  reserved            = true
-  
-  sku {    
-    tier = "Standard"
-    size = "S1"
-  }
+  location            = "eastus"
+  os_type                = "Linux"
+  sku_name		= "S1"
 }
+
+resource "azurerm_linux_web_app" "example" {
+  name                = "docker-app-service"
+  resource_group_name = "tfex120"
+  location            = "eastus"
+  service_plan_id     = azurerm_service_plan.main.id
+  site_config {}
+}
+
+/*
 resource "azurerm_app_service" "main" {
   name                = "docker-app-service"
   location            = "eastus"
   resource_group_name = "tfex120"
-  app_service_plan_id = "${azurerm_app_service_plan.main.id}"
+  app_service_plan_id = "${azurerm_service_plan.main.id}"
 }
+*/
